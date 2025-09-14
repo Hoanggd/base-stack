badc7fdc011e4e8965cbfc96da4a874aff733a8a
henry <henry.pham@sotatek.com>
2025-09-14 20:40:16 +0700

    fix: sonner

--- apps/docs/app/providers.tsx
+++ apps/docs/app/providers.tsx
@@ -4,7 +4,6 @@ import isoWeek from "dayjs/plugin/isoWeek";
 import { ThemeProvider } from "next-themes";
 import { useRouter } from "next/navigation";
 import { RouterProvider } from "react-aria-components";
-import { Toaster as SonnerToaster } from "sonner";
 import { Toaster } from "@workspace/ui/components/sonner";
 import { I18nProvider as ReactAriaI18nProvider } from "react-aria-components";
 
@@ -31,7 +30,6 @@ export function Providers({ children }: { children: React.ReactNode }) {
           enableColorScheme
         >
           {children}
-          <SonnerToaster />
           <Toaster />
         </ThemeProvider>
       </ReactAriaI18nProvider>
